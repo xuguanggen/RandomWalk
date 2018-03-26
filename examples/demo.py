c759a624cd1e372b079189337f342629b4960b13
xuguanggen <ggxu.soochow@gmail.com>
2018-03-26 17:41:13 +0800

    update demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 import sys
+import numpy as np
+
 import random
 from random import randint
 #coding = utf-8
@@ -17,14 +19,54 @@ move = [
         ]
 
 
+def JudgeWalk(mark):
+    for i in range(mark.shape[0]):
+        for j in range(mark.shape[1]):
+            if mark[i][j] == 0:
+                return False
+    return True
+
+
 
 if __name__=='__main__':
     X = int(sys.argv[1])
     Y = int(sys.argv[2])
 
+    mark = np.zeros((X,Y),dtype=int)
     current_x = randint(0, X-1)
     current_y = randint(0, Y-1)
-    print("X length:\t"+str(X))
-    print("Y length:\t"+str(Y))
-    print("x:\t"+str(current_x))
-    print("y:\t"+str(current_y))
+
+    num = 0
+    while True:
+        moving_idx = randint(0,7)
+        next_x = current_x + move[moving_idx][0]
+        next_y = current_y + move[moving_idx][1]
+
+        while(next_x < 0 | next_y < 0 | next_x >= X | next_y >= Y):
+            moving_idx = randint(0,7)
+            next_x = current_x + move[moving_idx][0]
+            next_y = current_y + move[moving_idx][1]
+
+        mark[next_x][next_y] += 1
+
+        current_x = next_x
+        current_x = next_y
+        num += 1
+
+        label = JudgeWalk(mark)
+        if label:
+            print("所有的格子都访问用了:\t"+str(num)+" 次")
+            for i in range(X):
+                row_str = ''
+                for j in range(Y):
+                    row_str += str(mark[i][j])+' ,'
+                print(row_str[:-1])
+
+        if num > 50000000:
+            print("超过了运动次数！！！")
+            for i in range(X):
+                row_str = ''
+                for j in range(Y):
+                    row_str += str(mark[i][j])+' ,'
+                print(row_str[:-1])
+            exit()
